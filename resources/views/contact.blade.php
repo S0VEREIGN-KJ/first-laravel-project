27c27dd95f7209c01e10a2723a7dc017e4cca320
S0VEREIGN-KJ <karljasper1231@gmail.com>
2025-04-08 09:40:32 +0800

    new

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
     <main class="container mx-auto mt-6 flex justify-center">
         <section class="w-3/5 bg-white p-6 shadow-md rounded-lg">
             <h1 class="text-2xl font-bold mb-4">Contact us</h1>
-            <p class="mt-4 text-lg text-gray-600">Email: support@website.com</p>
+            <p class="mt-4 text-lg text-gray-600">Email: karljasper1231@website.com</p>
         </section>
     </main>
 @endsection
